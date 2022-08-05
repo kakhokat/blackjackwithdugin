feab2947210a63a7ebd514a9ec65b159f86a0db4
kakhokat <57629970+kakhokat@users.noreply.github.com>
2022-08-05 09:41:54 +0300

    Update main.py

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ def work():
         if message.text == 'Начать игру':
             gamestarted = True
             bot.send_sticker(message.chat.id, 'CAACAgIAAxkBAAEFdTZi6owTRYiUU9O1zI8YfWUdLeSMVQACTwAD2xPJCQE3k6xcb3b0KQQ', reply_markup=types.ReplyKeyboardRemove())
-            deck = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48]
+            deck = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52]
             shuffle(deck)
             dugin_hand = []
             while calkulate_score(dugin_hand) < 17:
